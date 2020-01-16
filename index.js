3b117a501d5d954e89048ea82659ec1f6892759c
Christopher Spraggett <cspraggett@gmail.com>
2020-01-16 15:39:16 -0500

    fetchCoordsByIp works correctly when run from index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const { fetchMyIP } = require("./iss");
+const { fetchMyIP, fetchCoordsByIP } = require("./iss");
+
+// 66.207.199.230
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -6,4 +8,12 @@ const { fetchMyIP } = require("./iss");
 //     return;
 //   }
 //   console.log("It worked!", ip);
+// });
+
+// fetchCoordsByIP("66.207.199.230", (error, data) => {
+//   if (error) {
+//     console.log("It didn't work!", error.message);
+//   } else {
+//     console.log(data);
+//   }
 // });
--- iss.js
+++ iss.js
@@ -1,33 +1,56 @@
 const request = require("request");
 
-const ip = "https://api.ipify.org/?format=json";
+const getLocation = `https://ipvigilante.com/json/`;
+const getIp = "https://api.getIpify.org/?format=json";
 /**
- * Makes a single API request to retrieve the user's IP address.
+ * Makes a single API request to retrieve the user's getIp address.
  * Input:
- *   - A callback (to pass back an error or the IP string)
+ *   - A callback (to pass back an error or the getIp string)
  * Returns (via Callback):
  *   - An error, if any (nullable)
- *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ *   - The getIp address as a string (null if error). Example: "162.245.144.188"
  */
-const fetchMyIP = callback => {
-  request(ip, (error, response, body) => {
+const fetchMyIp = callback => {
+  request(getIp, (error, response, body) => {
     if (error) {
       callback(error, null);
       return;
     }
     if (response.statusCode !== 200) {
-      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      const msg = `Status Code ${response.statusCode} when fetching getIp. Response: ${body}`;
       callback(Error(msg), null);
       return;
     }
-    const data = JSON.parse(body).ip;
+    const data = JSON.parse(body).getIp;
     callback(null, data);
   });
 };
 
-// request(ip, (error, response, body) => {
-//   console.log("in fetchMyIP");
-//   console.log(body);
+const fetchCoordsByIP = (ip, callback) => {
+  request(`${getLocation}${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const [latitude, longitude] = [
+      JSON.parse(body).data.latitude,
+      JSON.parse(body).data.longitude
+    ];
+    callback(null, { latitude, longitude });
+  });
+};
+
+module.exports = { fetchMyIp, fetchCoordsByIP };
+
+// request(`${getLocation}66.207.199.230`, (error, response, body) => {
+//   const [latitude, longitude] = [
+//     JSON.parse(body).data.latitude,
+//     JSON.parse(body).data.longitude
+//   ];
+//   console.log(`lat: ${latitude}, lng: ${longitude}`);
 // });
-module.exports = { fetchMyIP };
-//request();
